e8294017e894c22db3185dfcc063208e55a61a64
malaahassan <malaa575@gmail.com>
2025-04-08 21:53:48 +0200

    Create 1.py

---
+++ section5/1.py
@@ -0,0 +1,4 @@
+print(list(range(6)))                # [0, 1, 2, 3, 4, 5]
+print(list(range(4, 12, 2)))         # [4, 6, 8, 10]
+print(list(range(10, 5, -1)))        # [10, 9, 8, 7, 6]
+print(list(range(10, -15, -5)))        # [10, 5, 0, -5, -10]
