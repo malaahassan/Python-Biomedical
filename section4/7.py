faa3f10f4551079d5ead5cce65eb9bca8611fa15
malaahassan <malaa575@gmail.com>
2025-04-08 21:55:40 +0200

    Delete section4/7.py

--- section4/7.py
+++
@@ -1,7 +0,0 @@
-numbers1 = [10, 20, 30]
-numbers2 = [10, 20, 30, 10, 20, 30]
-
-numbers1_len = len(set(numbers1))
-numbers2_len = len(set(numbers2))
-
-print(f"numbers1: {numbers1_len}, numbers2: {numbers2_len}")
